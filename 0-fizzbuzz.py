f6d942c59efb221049ba95759bddc34a81196179
Samuel Karmole <sambeti2002@gmail.com>
2023-12-12 12:02:56 -0800

    0-fizzbuzz.py

---
+++ 0-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/phyton3
+# FizzBuzz implementation
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+# Test FizzBuzz for 15
+print(fizz_buzz(15))
